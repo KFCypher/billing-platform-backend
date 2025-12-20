d03e252491af983c8c7f6b25c4251ec46f98e18d
Kelvin <thekelvinfordjour@gmail.com>
2025-12-20 07:50:55 +0000

    Add CORS_ALLOW_METHODS and preflight settings

--- config/settings.py
+++ config/settings.py
@@ -207,6 +207,14 @@ CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[
     'http://127.0.0.1:3000',
 ])
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 CORS_ALLOW_HEADERS = [
     'accept',
     'accept-encoding',
@@ -218,6 +226,9 @@ CORS_ALLOW_HEADERS = [
     'x-csrftoken',
     'x-requested-with',
     'x-api-key',
+    'x-tenant-id',
+]
+CORS_PREFLIGHT_MAX_AGE = 86400
     'x-tenant-id',
 ]
 
